a573dd19e542de93197240c47203394ea65106b7
kumasandesu <papaofarm.mk3@icloud.com>
2020-08-31 10:25:40 +0900

    ログイン機能　設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_for :users
+
+  devise_scope :users do
+    get '/users', to: redirect("/users/sign_up")
+  end
+
 end
