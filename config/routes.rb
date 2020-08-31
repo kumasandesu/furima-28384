0b9e1d1529109ea35ca07fd5ccf793aede649cd1
kumasandesu <papaofarm.mk3@icloud.com>
2020-08-31 14:13:14 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-
+  root 'items#index'
   devise_for :users
 
   devise_scope :users do
     get '/users', to: redirect("/users/sign_up")
   end
+
+  resources :items, only: [:index, :new, :showte, :crea]
 
 end
