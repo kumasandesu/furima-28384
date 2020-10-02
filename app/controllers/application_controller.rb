e1dbb042ce8cec27f82f4a928de4f7dacb2b3f92
kumasandesu <papaofarm.mk3@icloud.com>
2020-10-02 13:38:24 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
-    added_attrs = [ :nickname, :email, :password, :repassword, :name, :namekana, :birthday ]
-    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :birthday, :first_name_kana, :family_name_kana, :first_name, :family_name])
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'uziie' && password == '1234'
+    end
   end
 end
