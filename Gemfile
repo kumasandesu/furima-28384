87f77d400e47a22a377742f5ded87ec5c87a9ac9
kumasandesu <papaofarm.mk3@icloud.com>
2020-10-02 14:40:49 +0900

    修正

--- Gemfile
+++ Gemfile
@@ -53,6 +53,14 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
+group :development do
+  gem 'rubocop', require: false
+end
+
 gem 'devise'
 
 gem 'active_hash'
+
+gem 'payjp'
+
+gem 'faker'
--- db/migrate/20200903062100_create_items.rb
+++ db/migrate/20200903062100_create_items.rb
@@ -1,15 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string          :item_image         , null: false
-      t.string          :item_name          , null: false
-      t.text            :item_info          , null: false
-      t.integer         :category_id        , null: false
-      t.integer         :status_id          , null: false
-      t.integer         :shippingfee_id     , null: false
-      t.integer         :prefecture_id      , null: false
-      t.integer         :scheduled_id       , null: false
-      t.integer         :item_price         , null: false
+      t.string         :name               , null: false
+      t.integer        :category_id        , null: false
+      t.integer        :condition_id       , null: false
+      t.integer        :delivery_fee_id    , null: false
+      t.integer        :ship_from_id       , null: false
+      t.integer        :days_until_ship_id , null: false
+      t.text           :content            , null: false
+      t.integer        :price              , hull: false
+      t.references     :user               , null: false
       t.timestamps
     end
   end
